52bafc07cccf5c976b8825f0b581fd54832c87aa
chaitanya teja <chaitanyateja_golla@intuit.com>
2017-04-01 16:34:04 -0700

    Adding repo for Movie Viewer

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Golla, Chaitanya Teja on 4/1/17.
+//  Copyright © 2017 Golla, Chaitanya Teja. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MovieViewer/MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  MovieViewer
+//
+//  Created by Golla, Chaitanya Teja on 4/1/17.
+//  Copyright © 2017 Golla, Chaitanya Teja. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
